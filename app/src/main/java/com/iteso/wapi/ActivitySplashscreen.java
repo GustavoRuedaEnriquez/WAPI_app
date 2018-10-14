fa064f3e3cc145d728bd3803f8c679b1515092ea
unknown <gusgus2208@gmail.com>
2018-10-14 15:04:12 -0500

    Splashscreen added.

---
+++ app/src/main/java/com/iteso/wapi/ActivitySplashscreen.java
@@ -0,0 +1,35 @@
+package com.iteso.wapi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ActivitySplashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splashscreen);
+
+        TimerTask task= new TimerTask(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(ActivitySplashscreen.this,ActivityMain.class);
+                startActivity(intent);
+                finish();
+
+                //FALTA HACER EL LOGIN, DE ESO ME ENCARGO YO.
+
+
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 2000);
+
+
+    }
+}
