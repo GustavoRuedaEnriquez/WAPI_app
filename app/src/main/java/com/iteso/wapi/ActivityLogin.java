2a427592da8abcd2709ee23cc2f848cbc470fbed
unknown <gusgus2208@gmail.com>
2018-10-16 17:41:02 -0500

    Login created

--- app/src/main/java/com/iteso/wapi/ActivityLogin.java
+++ app/src/main/java/com/iteso/wapi/ActivityLogin.java
@@ -1,13 +1,47 @@
 package com.iteso.wapi;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ActivityLogin extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.activity_login_user_et);
+        password = findViewById(R.id.activity_login_password_et);
+        login = findViewById(R.id.activity_login_access_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login.setBackground(getDrawable(R.drawable.custom_selected_blue_light_btn));
+                login.setTextColor(Color.WHITE);
+                savePreferences();
+                Intent intent = new Intent(ActivityLogin.this,ActivityMain.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void savePreferences(){
+        SharedPreferences sharedPreferences = getSharedPreferences(ActivitySplashscreen.MY_PREFERENCES,MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("NAME", username.getText().toString());
+        editor.putString("PWD", password.getText().toString());
+        editor.putBoolean("LOGGED", true);
+        editor.apply();
     }
 }
