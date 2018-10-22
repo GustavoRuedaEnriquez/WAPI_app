758c8ce7f7097594f64fe6ea6cf2b96aa58be4a8
unknown <gusgus2208@gmail.com>
2018-10-22 12:06:39 -0500

    Merged feature_configuration_

--- app/src/main/java/com/iteso/wapi/ActivitySplashscreen.java
+++ app/src/main/java/com/iteso/wapi/ActivitySplashscreen.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.iteso.wapi.beans.Student;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
--- app/src/main/java/com/iteso/wapi/AdapterMateria.java
+++ app/src/main/java/com/iteso/wapi/AdapterMateria.java
@@ -7,9 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
-
 import com.iteso.wapi.beans.Materia;
-
 import java.util.List;
 
 public class AdapterMateria extends RecyclerView.Adapter<AdapterMateria.MyViewHolder>{
